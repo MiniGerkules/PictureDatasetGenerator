bbd600300c322457975f5b164e8d876f9414576d
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2023-09-03 21:31:16 +0300

    Add data class for parameters of strokes

---
+++ src/main/kotlin/strokes/StrokeParameters.kt
@@ -0,0 +1,10 @@
+package strokes
+
+data class StrokeParameters(
+    val length: Double,
+    val straightness: Double,
+    val orientation : Double,
+    val nbs_nb : UInt,       // Number of brushstrokes in the neighborhood
+    val nbs_so : UInt,       // Number of brushstrokes with similar orientations in the neighborhood
+    val osd_nb : Double,     // Orientation standard deviation in the neighborhood
+)
