8f112e98c37e99f5fcb46e48666891b1063f5285
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2023-09-05 00:22:02 +0300

    Add function to count length between 2 vertexes

--- src/main/kotlin/stroke/Vertex.kt
+++ src/main/kotlin/stroke/Vertex.kt
@@ -2,6 +2,16 @@ package stroke
 
 import java.awt.Color
 
+import kotlin.math.pow
+import kotlin.math.sqrt
+
 data class Vertex(val position: Position,
                   val radius: Double,
                   val color: Color)
+
+fun getLength(a: Vertex, b: Vertex): Double {
+    return sqrt(
+        (a.position.x - b.position.x).pow(2.0) +
+                (a.position.y - b.position.y).pow(2.0)
+    )
+}
